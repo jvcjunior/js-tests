1b0954403092ee225d530e79c73f67cd1bf10d1a
Valter junior <bloodsportjr@gmail.com>
2017-05-08 16:02:55 -0300

    Add sparklines charts

--- React/2017/udemy/react-redux-course-weather-forecast/src/containers/weather_list.js
+++ React/2017/udemy/react-redux-course-weather-forecast/src/containers/weather_list.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
     renderWeather(cityData){
+        const name          = cityData.city.name;
+        const temps         = cityData.list.map(weather => weather.main.temp);
+        //const humidities    = cityData.list.map(weather => {weather.main.humidity});
+        //const pressures     = cityData.list.map(weather => {weather.main.pressure});
+
         return(
-            <tr>
-                <td>{cityData.city.name}</td>
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <Sparklines height={120} width={180} data={temps}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
+                </td>
             </tr>
         );
     }
