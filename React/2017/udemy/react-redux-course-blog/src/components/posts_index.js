d962ad8e3185e9e63168cd20348f3e3eff8622b8
Valter junior <bloodsportjr@gmail.com>
2017-05-11 17:06:58 -0300

    add react router

--- React/2017/udemy/react-redux-course-blog/src/components/posts_index.js
+++ React/2017/udemy/react-redux-course-blog/src/components/posts_index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect }  from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -9,7 +10,6 @@ class PostsIndex extends Component {
     }
 
     renderPosts() {
-        console.log('this.props.posts', this.props.posts)
        return _.map(this.props.posts, post => {
          return (
             <li className="list-group-item" key={post.id}>
@@ -22,6 +22,11 @@ class PostsIndex extends Component {
     render() {
         return (
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
---
+++ React/2017/udemy/react-redux-course-blog/src/components/posts_new.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    render() {
+        return (
+            <form action="">
+                <Field
+                    name="title"
+                    component=""
+                />
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
