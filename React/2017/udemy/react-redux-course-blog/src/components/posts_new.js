7b1576d167d02b718d834f21362977aa7aea2b83
Valter junior <bloodsportjr@gmail.com>
2017-05-12 10:25:20 -0300

    Add new post page inputs

--- React/2017/udemy/react-redux-course-blog/src/components/posts_new.js
+++ React/2017/udemy/react-redux-course-blog/src/components/posts_new.js
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return (
-            <form action="">
+            <form>
                 <Field
+                    label="Title"
                     name="title"
-                    component=""
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
                 />
             </form>
         );
