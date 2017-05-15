eb8e026571d04f375af8f38d1b6618f871c7a3a4
Valter junior <bloodsportjr@gmail.com>
2017-05-15 14:54:31 -0300

    Finish project from react course

--- React/2017/udemy/react-redux-course-blog/src/components/posts_new.js
+++ React/2017/udemy/react-redux-course-blog/src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions'
 
 class PostsNew extends Component {
     renderField(field) {
@@ -24,7 +26,9 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -73,4 +77,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
--- React/2017/udemy/react-redux-course-blog/src/reducers/reducer_posts.js
+++ React/2017/udemy/react-redux-course-blog/src/reducers/reducer_posts.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
-import  { FETCH_POSTS } from '../actions';
+import  { FETCH_POSTS, CREATE_POST , FETCH_POST, DELETE_POST} from '../actions';
 
 export default function(state = {}, action){
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case CREATE_POST:
+            console.log('creating...');
+        case FETCH_POST:
+            return {...state, [action.payload.data.id]: action.payload.data };
+        case CREATE_POST:
+            return _.omit(state, action.payload);
         default:
             return state;
     }
